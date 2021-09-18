f94623287839100637750f7a4919bc19ec94c476
shuhua_zhang <shuhua_zhang@fzzixun.com>
2021-09-18 18:57:22 +0800

    init

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: shuhua
+ * @Date: 2021-09-18 13:54:24
+ * @LastEditTime: 2021-09-18 13:54:24
+ * @LastEditors: shuhua
+ * @Description:
+ * @FilePath: \myNewManage\src\api\index.js
+ */
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: shuhua
+ * @Date: 2021-09-18 13:57:19
+ * @LastEditTime: 2021-09-18 13:57:20
+ * @LastEditors: shuhua
+ * @Description:
+ * @FilePath: \myNewManage\vue.config.js
+ */
